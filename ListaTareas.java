62f04f5e348f0760f6c00a0cdaefe9d2c9f5f91e
JessTDS <143725020+JessTDS@users.noreply.github.com>
2023-09-05 20:54:14 -0600

    Add files via upload

---
+++ ListaTareas.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package unidad2;
+
+/**
+ *
+ * @author ponch
+ */
+public class ListaTareas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String tarea1 = "lavar la ropa";
+        String tarea2 = "exposicion";
+        String tarea3 = "operaciones matematicas";
+        String tarea4 = "tender la cama";
+        String tarea5 = "resumen del texto";
+        
+        System.out.println("La primera tarea es " + tarea1);
+        System.out.println("La segunda tarea es " + tarea2);
+        System.out.println("La tercera tarea es " + tarea3);
+        System.out.println("La cuarta tarea es " + tarea4);
+        System.out.println("La quinta tarea es " + tarea5);
+    }
+    
+}
