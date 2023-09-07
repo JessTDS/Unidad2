b2bf74bb63ba4d8a1daab121f4d84d1769683a21
JessTDS <143725020+JessTDS@users.noreply.github.com>
2023-09-07 08:46:45 -0600

    Add files via upload

---
+++ Practica2JTDS.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operaciones;
+
+/**
+ *
+ * @author labcca
+ */
+public class Practica2 {
+    
+    public static void main(String[] args) {
+        
+        String nombre = "Jessica Torres De Sixto";
+        
+        int base = 22;
+        int altura = 12;
+        int areaRectangulo = base * altura;
+        
+        System.out.println("Mi nombre completo es: " + nombre);
+        System.out.println("El area del rectangulo es: " + areaRectangulo);
+    }
+    
+}
